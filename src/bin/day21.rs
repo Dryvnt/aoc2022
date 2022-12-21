f8cb60e1d8171af2e833bdf765dfc76a0ffd3f5f
Thomas Gravgaard Hansen <tgh@gravgaard.dev>
2022-12-21 12:39:55 +0100

    Refactor day 21

--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -1,6 +1,12 @@
-use std::{collections::HashMap, fs};
+use std::{
+    cell::RefCell,
+    collections::HashMap,
+    fs,
+    ops::Index,
+    sync::atomic::{AtomicU64, Ordering},
+};
 
-use anyhow::{anyhow, bail, Error};
+use anyhow::{anyhow, bail, Context, Error};
 use nom::{
     bytes::complete::tag,
     character::complete::{alpha1, digit1, one_of},
@@ -9,31 +15,53 @@ use nom::{
     IResult,
 };
 
-#[derive(Clone, Copy)]
 enum Monkey<'a> {
     Const(u64),
     Operation {
-        a: &'a str,
-        b: &'a str,
+        left: &'a str,
+        right: &'a str,
         op: Operation,
     },
 }
 
-impl<'a> Monkey<'a> {
-    fn left_right(&self) -> Result<(&'a str, &'a str), Error> {
-        Ok(match self {
-            Monkey::Const(_) => bail!("root is const"),
-            Monkey::Operation { a, b, op: _ } => (a, b),
-        })
-    }
-}
-
-#[derive(Clone, Copy)]
 enum Operation {
     Add,
     Sub,
     Mul,
     Div,
+    Eq,
+}
+
+impl Operation {
+    fn solve_for_left(&self, value: u64, right: u64) -> u64 {
+        match self {
+            Operation::Add => value - right,
+            Operation::Sub => value + right,
+            Operation::Mul => value / right,
+            Operation::Div => value * right,
+            Operation::Eq => right,
+        }
+    }
+
+    fn solve_for_right(&self, value: u64, left: u64) -> u64 {
+        match self {
+            Operation::Add => value - left,
+            Operation::Sub => left - value,
+            Operation::Mul => value / left,
+            Operation::Div => left / value,
+            Operation::Eq => left,
+        }
+    }
+
+    fn eval(&self, left: u64, right: u64) -> u64 {
+        match self {
+            Operation::Add => left + right,
+            Operation::Sub => left - right,
+            Operation::Mul => left * right,
+            Operation::Div => left / right,
+            Operation::Eq => unimplemented!("cannot eval eq operation"),
+        }
+    }
 }
 
 fn parse_monkey(s: &str) -> Result<(&str, Monkey), Error> {
@@ -46,7 +74,8 @@ fn parse_monkey(s: &str) -> Result<(&str, Monkey), Error> {
             return Ok((s, (name, Monkey::Const(c))));
         }
 
-        let (s, (a, _, op, _, b)) = tuple((alpha1, tag(" "), one_of("+-*/"), tag(" "), alpha1))(s)?;
+        let (s, (left, _, op, _, right)) =
+            tuple((alpha1, tag(" "), one_of("+-*/"), tag(" "), alpha1))(s)?;
 
         let op = match op {
             '+' => Operation::Add,
@@ -56,7 +85,7 @@ fn parse_monkey(s: &str) -> Result<(&str, Monkey), Error> {
             _ => unreachable!(),
         };
 
-        Ok((s, (name, Monkey::Operation { a, b, op })))
+        Ok((s, (name, Monkey::Operation { left, right, op })))
     }
 
     let (_, m) = parse(s).map_err(|e| anyhow!("could not parse monkey: {:?}", e))?;
@@ -64,132 +93,87 @@ fn parse_monkey(s: &str) -> Result<(&str, Monkey), Error> {
     Ok(m)
 }
 
-struct MonkeyContext<'a> {
+struct MonkeyCollection<'a> {
     monkeys: HashMap<&'a str, Monkey<'a>>,
-    value_cache: HashMap<&'a str, u64>,
-    has_human_cache: HashMap<&'a str, bool>,
 }
 
-impl<'a> MonkeyContext<'a> {
+impl<'a> MonkeyCollection<'a> {
     fn parse_input(input: &'a str) -> Result<Self, Error> {
         let monkeys = input
             .lines()
             .map(parse_monkey)
             .collect::<Result<HashMap<_, _>, _>>()?;
 
-        Ok(MonkeyContext {
-            monkeys,
-            value_cache: HashMap::new(),
-            has_human_cache: HashMap::new(),
+        Ok(MonkeyCollection { monkeys })
+    }
+
+    fn get(&self, which: &str) -> Result<&Monkey, Error> {
+        self.monkeys.get(which).context("could not find monkey")
+    }
+
+    // Will be called once on each monkey, literally no gain from memoizing
+    fn get_value(&self, which: &'a str) -> Result<u64, Error> {
+        match self.get(which)? {
+            Monkey::Const(c) => Ok(*c),
+            Monkey::Operation { left, right, op } => {
+                let left = self.get_value(left)?;
+                let right = self.get_value(right)?;
+                Ok(op.eval(left, right))
+            }
+        }
+    }
+
+    // Will be called multiple times on the same monkey, but not worth memoizing
+    fn has_human(&self, which: &'a str) -> Result<bool, Error> {
+        if which == "humn" {
+            return Ok(true);
+        }
+
+        Ok(match self.get(which)? {
+            Monkey::Operation { left, right, op: _ } => {
+                self.has_human(left)? || self.has_human(right)?
+            }
+            _ => false,
         })
     }
 
-    fn get_value(&mut self, which: &'a str) -> u64 {
-        if self.value_cache.contains_key(which) {
-            return self.value_cache[which];
-        }
-        let m = self.monkeys[which];
-        let v = match m {
-            Monkey::Const(c) => c,
-            Monkey::Operation { a, b, op } => {
-                let a = self.get_value(a);
-                let b = self.get_value(b);
-                match op {
-                    Operation::Add => a + b,
-                    Operation::Sub => a - b,
-                    Operation::Mul => a * b,
-                    Operation::Div => a / b,
-                }
-            }
-        };
-        self.value_cache.insert(which, v);
-        v
-    }
-
-    fn has_human(&mut self, which: &'a str) -> bool {
-        if which == "humn" {
-            return true;
-        }
-
-        if self.has_human_cache.contains_key(which) {
-            return self.has_human_cache[which];
-        }
-        let v = match self.monkeys[which] {
-            Monkey::Operation { a, b, op: _ } => self.has_human(a) || self.has_human(b),
-            _ => false,
-        };
-        self.has_human_cache.insert(which, v);
-        v
-    }
-
-    fn human_value_to_make_eq(&mut self, which: &'a str, value: u64) -> Result<u64, Error> {
+    fn solve_for_human(&self, which: &'a str, value: u64) -> Result<u64, Error> {
         if which == "humn" {
             return Ok(value);
         }
 
-        if !self.has_human(which) {
-            bail!("wrong subtree!");
-        }
+        if let Monkey::Operation { left, right, op } = &self.monkeys[which] {
+            let op = if which == "root" { &Operation::Eq } else { op };
+            let left_has_human = self.has_human(left)?;
+            debug_assert_eq!(self.has_human(right)?, !left_has_human);
 
-        let m = self.monkeys[which];
-        if let Monkey::Operation {
-            a: left,
-            b: right,
-            op,
-        } = m
-        {
-            if self.has_human(left) {
-                debug_assert!(self.has_human(left));
-                let right = self.get_value(right);
-                let to_match = match op {
-                    Operation::Add => value - right, // value = left + right, left = value - right
-                    Operation::Sub => value + right, // value = left - right, left = value + right
-                    Operation::Mul => value / right, // value = left * right, left = value / right
-                    Operation::Div => value * right, // value = left / right, left = value * right
-                };
-
-                self.human_value_to_make_eq(left, to_match)
+            if left_has_human {
+                let right = self.get_value(right)?;
+                let target_value = op.solve_for_left(value, right);
+                self.solve_for_human(left, target_value)
             } else {
-                debug_assert!(self.has_human(right));
-                let left = self.get_value(left);
-                let to_match = match op {
-                    Operation::Add => value - left, // value = left + right, right = value - left
-                    Operation::Sub => left - value, // value = left - right, right = - (value - left)
-                    Operation::Mul => value / left, // value = left * right, right = value / left
-                    Operation::Div => left / value, // value = left / right, right = left / value
-                };
-
-                self.human_value_to_make_eq(right, to_match)
+                let left = self.get_value(left)?;
+                let target_value = op.solve_for_right(value, left);
+                self.solve_for_human(right, target_value)
             }
         } else {
-            bail!("monkey has no children")
+            bail!("monkey has no subtree")
         }
     }
 }
 
 fn part1(input: &str) -> Result<u64, Error> {
-    let mut ctx = MonkeyContext::parse_input(input)?;
+    let monkeys = MonkeyCollection::parse_input(input)?;
 
-    Ok(ctx.get_value("root"))
+    monkeys.get_value("root")
 }
 
 fn part2(input: &str) -> Result<u64, Error> {
-    let mut ctx = MonkeyContext::parse_input(input)?;
+    let monkeys = MonkeyCollection::parse_input(input)?;
 
-    let root = ctx.monkeys["root"];
-    let (left, right) = root.left_right()?;
+    let root_value = monkeys.get_value("root")?;
 
-    let (to_solve, value) = if ctx.has_human(left) {
-        (left, right)
-    } else {
-        (right, left)
-    };
-
-    let value = ctx.get_value(value);
-
-    let v = ctx.human_value_to_make_eq(to_solve, value)?;
-
-    Ok(v)
+    monkeys.solve_for_human("root", root_value)
 }
 
 fn main() -> Result<(), Error> {
